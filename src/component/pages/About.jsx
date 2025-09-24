17d7281de5c5f2459afaac737636f198d8747b81
Zeeshan Raza <zeeshanrahman72@gmail.com>
2025-09-24 23:17:38 +0530

    About jsx file created

--- src/component/pages/About.jsx
+++ src/component/pages/About.jsx
@@ -1,24 +1,58 @@
 export default function About() {
   return (
-    <div>
-      <h1>
-        About <strong>ZeeFlix</strong>
-      </h1>
-      <p>
-        About Rotten Tomatoes® As the world’s most trusted and recognized source
-        of movie and TV reviews, Rotten Tomatoes and the Tomatometer score have
-        served as the most reliable home of entertainment recommendations for
-        over 25 years. We offer authentic reactions in a broad range of reviews
-        from professional critics and everyday fans alike in addition to
-        helpful, easily digestible what-to-watch roundups. Not only that, but we
-        help build anticipation and provide fans with access to first look
-        entertainment previews, trailers, behind-the-scenes exclusives, and
-        more, as well as original editorial features, fun and informative video
-        series, and even books and games. If you're an entertainment fan looking
-        for a diverse range of perspectives curated in an easy-to-understand
-        format, we'll help you find what you're in the mood for and open your
-        eyes to new things to enjoy
-      </p>
+    <div className="container-fluid my-5">
+      <div className="card bg-black  m-5">
+        <h1>
+          About <strong>ZeeFlix</strong>
+        </h1>
+        <p>
+          About <strong>ZeeFlix®</strong> As the world’s most trusted and
+          recognized source of movie and TV reviews, Rotten Tomatoes and the
+          Tomatometer score have served as the most reliable home of
+          entertainment recommendations for over 25 years. We offer authentic
+          reactions in a broad range of reviews from professional critics and
+          everyday fans alike in addition to helpful, easily digestible
+          what-to-watch roundups. Not only that, but we help build anticipation
+          and provide fans with access to first look entertainment previews,
+          trailers, behind-the-scenes exclusives, and more, as well as original
+          editorial features, fun and informative video series, and even books
+          and games. If you're an entertainment fan looking for a diverse range
+          of perspectives curated in an easy-to-understand format, we'll help
+          you find what you're in the mood for and open your eyes to new things
+          to enjoy
+        </p>
+        <h2>
+          What is the Zee<em>Meter</em> ?
+        </h2>
+        <p>
+          The Zee<em>Meter</em>, which captures audience sentiment, is
+          represented by a popcorn bucket and indicates the percentage of fans
+          who have rated a movie or TV show positively. When we can verify users
+          have bought a ticket to a film, the default score shown is composed of
+          “Verified Ratings.” For all other titles, we display an “All” score
+          that includes ratings from people regardless of whether or not we can
+          verify that they have seen the movie. Titles displaying Verified
+          Ratings also have an All score. To view it, simply click on the
+          popcorn bucket and you can toggle between the two.
+        </p>
+        <h2>Community & Critics Code of Conduct</h2>
+        <p>
+          At ZeeFlix, we are always looking to update our policies to try to
+          ensure we maintain a safe and fair community for every movie and TV
+          fan. For our Community Code of Conduct, click HERE. You can also find
+          our Tomatometer-approved Critics Code of Conduct HERE.
+        </p>
+        <h2>Curation</h2>
+        <p>
+          <strong>ZeeFlix®</strong> has a team of curators whose job is to
+          gather thousands of movie and TV reviews weekly. The team constantly
+          collects movie and TV reviews from Tomatometer-approved critics and
+          publications generating Tomatometer scores. Our curation process
+          considers these reviews, noting if they are Fresh or Rotten, and our
+          curators choose a representative pull-quote. Tomatometer-approved
+          critics can also self-submit their reviews.
+        </p>
+      </div>
     </div>
   );
 }
