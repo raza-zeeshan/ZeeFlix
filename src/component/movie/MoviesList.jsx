d7a8cefd74a8deeca79184eeff14d09fc7c4864f
Zeeshan Raza <zeeshanrahman72@gmail.com>
2025-09-28 21:54:29 +0530

    Movie List Modified

--- src/component/movie/MoviesList.jsx
+++ src/component/movie/MoviesList.jsx
@@ -7,6 +7,7 @@ import Shimmer from "../pages/Shimmer";
 export default function MoviesList() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isError, setIsError] = useState("");
 
   useEffect(() => {
     fetchMovies();
@@ -15,15 +16,17 @@ export default function MoviesList() {
   async function fetchMovies() {
     try {
       const response = await axios.get(
-        "http://www.omdbapi.com/?s=superman&apikey=2adcf282"
+        `http://www.omdbapi.com/?s=superman&apikey=2adcf282`
       );
       const data = response.data;
       setMovies(data.Search);
       console.log(data);
       setLoading(false);
+      setIsError("");
     } catch (error) {
       console.error("Error fetching movies:", error);
       setLoading(false);
+      setIsError("Failed to fetch movies. Please try again later.");
     }
     console.log(movies);
   }
@@ -33,7 +36,13 @@ export default function MoviesList() {
       <div className="container-fluid">
         <div className="row">
           {loading && <Shimmer />}
+          {!loading && isError && (
+            <div className="col-10 m-auto text-center">
+              <h2 className="text-danger my-5">{isError}</h2>
+            </div>
+          )}
           {!loading &&
+            !isError &&
             movies.map((movie) => (
               <div className="col-3 text-center my-4 " key={movie.imdbID}>
                 <Link to={`/movies/${movie.imdbID}`}>
